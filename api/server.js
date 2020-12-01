62131b3f6df0cedaa7dd9f6c2cbbefafb55dfbda
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-01 14:40:24 +0100

    change to dogs and adopters

--- api/server.js
+++ api/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const adoptersRouter = require('./adopters/adopters-router');
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/hubs', hubsRouter); //delegate requests to /api/hubs to the router
+
+server.use('/api/adopters', adoptersRouter); // delegate requests to /api/adopters to the router
 
 server.get('/', (req, res) => {
   res.send(`
-    <h2>Lambda Hubs API</h>
-    <p>Welcome to the Lambda Hubs API</p>
+    <h2>Lambda Shelter API</h>
+    <p>Welcome to the Lambda Shelter API</p>
   `);
 });
 
---
+++ data/seeds/001-adopters.js
@@ -0,0 +1,7 @@
+exports.seed = function(knex) {
+  return knex('adopters').insert([
+    { name: 'Jane', email: 'jane@jane.com' },
+    { name: 'Paul', email: 'paul@paul.com' },
+    { name: 'Sarah', email: 'sarah@sarah.com' },
+  ]);
+}
